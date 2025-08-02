78bffe0df33713412ce3fc03af44a6940fc80af7
Stefanie <stefanie.aleortega@gmail,com>
2025-08-02 16:43:38 -0500

    calculadora inicial con funciones suma, resta , multiplicación y división

---
+++ calculadora.cpp
@@ -0,0 +1,51 @@
+﻿// Creación de calculadora 
+#include <iostream>
+using namespace std;
+
+double sumar(double a, double b) {
+    return a + b;
+}
+
+double restar(double a, double b) {
+    return a - b;
+}
+
+double multiplicar(double a, double b) {
+    return a * b;
+}
+
+double dividir(double a, double b) {
+    if (b == 0) {
+        cout << "Error: División por cero no permitida." << endl;
+        return 0;
+    }
+    return a / b;
+}
+
+int main() {
+    "CALCULADORA GRUPO 1"
+    double num1, num2;
+    char operacion;
+    cout << "Ingrese dos números: ";
+    cin >> num1 >> num2;
+    cout << "Ingrese operación (+, -, *, /): ";
+    cin >> operacion;
+
+    switch (operacion) {
+        case '+':
+            cout << "Resultado: " << sumar(num1, num2) << endl;
+            break;
+        case '-':
+            cout << "Resultado: " << restar(num1, num2) << endl;
+            break;
+        case '*':
+            cout << "Resultado: " << multiplicar(num1, num2) << endl;
+            break;
+        case '/':
+            cout << "Resultado: " << dividir(num1, num2) << endl;
+            break;
+        default:
+            cout << "Operación no válida." << endl;
+    }
+    return 0;
+}
