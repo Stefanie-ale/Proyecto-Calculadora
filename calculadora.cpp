179c03b86361582ab2f4665a1fc7d54da2ef00e6
Juan Quinatoa <juan.quinatoa@epn.edu.ec>
2025-08-03 19:39:12 -0500

    Añadida función logaritmoNatural en rama logaritmoNatural

--- calculadora.cpp
+++ calculadora.cpp
@@ -1,5 +1,6 @@
 ﻿// Creación de calculadora 
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 double sumar(double a, double b) {
@@ -22,13 +23,21 @@ double dividir(double a, double b) {
     return a / b;
 }
 
+double logaritmoNatural(double a) {
+    if (a <= 0) {
+        cout << "Error: Logaritmo natural de número no positivo no permitido." << endl;
+        return 0;
+    }
+    return log(a);
+}
+
 int main() {
-    "CALCULADORA GRUPO 1"
+    //"CALCULADORA GRUPO 1"
     double num1, num2;
     char operacion;
     cout << "Ingrese dos números: ";
     cin >> num1 >> num2;
-    cout << "Ingrese operación (+, -, *, /): ";
+    cout << "Ingrese operación (+, -, *, /, n): ";
     cin >> operacion;
 
     switch (operacion) {
@@ -43,6 +52,9 @@ int main() {
             break;
         case '/':
             cout << "Resultado: " << dividir(num1, num2) << endl;
+            break;
+        case 'n':
+            cout << "Resultado: " << logaritmoNatural(num1) << endl;
             break;
         default:
             cout << "Operación no válida." << endl;
