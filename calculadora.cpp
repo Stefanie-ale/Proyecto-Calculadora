6c53bb570a1541228444e7ad69ccec1ba7686437
Gabriel Q <gabrielplayq@gmail.com>
2025-08-03 19:35:59 -0500

    Mejora en main de números y está agregada la función raíz cuadrada

---
+++ calculadora.cpp
@@ -0,0 +1,66 @@
+﻿// Creación de calculadora 
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+double sumar(double a, double b) {
+    return a + b;
+}
+
+double restar(double a, double b) {
+    return a - b;
+}
+
+double multiplicar(double a, double b) {
+    return a * b;
+}
+
+double dividir(double a, double b) {
+    if (b == 0) {
+        cout << "Error: División por cero no permitida." << endl;
+        return 0;
+    }
+    return a / b;
+}
+
+double raizCuadrada(double a) {
+    if (a < 0) {
+        cout << "Error: Raíz cuadrada de número negativo no permitida." << endl;
+        return 0;
+    }
+    return sqrt(a);
+}
+
+int main() {
+    cout << "CALCULADORA GRUPO 1" << endl;
+    double num1, num2;
+    char operacion;
+    cout << "Ingrese un número (para raíz cuadrada) o dos números (para otras operaciones): ";
+    cin >> num1;
+    if (cin.peek() != '\n') {
+        cin >> num2;
+    }
+    cout << "Ingrese operación (+, -, *, /,r): ";
+    cin >> operacion;
+
+    switch (operacion) {
+        case '+':
+            cout << "Resultado: " << sumar(num1, num2) << endl;
+            break;
+        case '-':
+            cout << "Resultado: " << restar(num1, num2) << endl;
+            break;
+        case '*':
+            cout << "Resultado: " << multiplicar(num1, num2) << endl;
+            break;
+        case '/':
+            cout << "Resultado: " << dividir(num1, num2) << endl;
+            break;
+        case 'r':
+            cout << "Resultado: " << raizCuadrada(num1) << endl;
+            break;
+        default:
+            cout << "Operación no válida." << endl;
+    }
+    return 0;
+}
